bf57c729e04a25535d41f3c5a2a9a8360d164f7f
LandRover <github.com@landroverz.com>
2019-09-26 08:09:59 +0300

    dashboard to run a background

--- vagrant/minikube.sh
+++ vagrant/minikube.sh
@@ -57,7 +57,7 @@ sudo -E minikube start \
 
 # enables dashboard
 sudo -E minikube addons enable dashboard
-sudo -E minikube dashboard 2>&1 &
+sudo -E minikube dashboard &>/dev/null &
 
 # either use sudo on all kubectl commands, or chown/chgrp to your user
 sudo chown -R ${USER}:${USER} /home/${USER}/.kube /home/${USER}/.minikube
